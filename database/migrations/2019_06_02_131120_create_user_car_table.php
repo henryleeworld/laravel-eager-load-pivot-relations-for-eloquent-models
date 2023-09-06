e37c687af5833aff17bd14a4418757006ad572be
Henry Lee <henryleeworld@gmail.com>
2023-09-07 00:13:33 +0800

    feat:升級至 laravel 10

--- database/migrations/2019_06_02_131120_create_user_car_table.php
+++ database/migrations/2019_06_02_131120_create_user_car_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserCarTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -31,4 +31,4 @@ class CreateUserCarTable extends Migration
     {
         Schema::dropIfExists('user_car');
     }
-}
+};
